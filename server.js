dcae89be6d6229695fdc9b26fef94f5964ea1918
harishuppu <97732041+harishuppu@users.noreply.github.com>
2023-09-21 03:06:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-//Allow all requests from all domains & localhost
+//Allow all requests from all domains & localhost .
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
